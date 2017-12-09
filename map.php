6bda9fdb51a76123359ae31fe61a678956b01896
engineer-pearl <theadavis101@gmail.com>
2017-12-09 14:53:40 -0800

    Adjusted emergency pages

--- map.php
+++ map.php
@@ -1,7 +1,7 @@
 <?php
     ##This is our list of maps
     $pictures = array(
-    "3" => array(
+    "1" => array(
         "https://csumb.edu/sites/default/files/images/st-block-31-1423244951922-raw-thirdfloor.jpg",
         "<ul><li>Turn left out the door. Continue to the end of the hallway.</li><li>Take the stairwell behind the blue door.</li></ul> Medical resources can be found in room 102",
         ),
@@ -9,7 +9,7 @@
     "https://csumb.edu/sites/default/files/images/st-block-31-1423245014139-raw-secondfloor.jpg", 
         "<ul><li>Turn left out the door. Continue to the end of the hallway.</li><li>Take the stairwell behind the blue door.</li></ul> Medical resources can be found in room 202", 
     ),
-    "1" => array(
+    "3" => array(
     "http://csumb.edu/sites/default/files/images/st-block-27-1423244904851-raw-firstfloor.jpg", 
         "<ul><li>Turn left out the door. Continue to the end of the hallway.</li><li>Take the stairwell behind the blue door.</li></ul> Medical resources can be found in room 302"
         )
@@ -20,9 +20,9 @@
         <?php $item = $pictures[$_GET['floor']] ?>
         <div style="float: right; width: 50%">
         <?php echo $item[1] ?>
-        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><?php include"key.php"?>
+        <!-- <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><?php include"key.php"?> -->
     </div>
-    <img src="<?php echo $item[0]; ?>" style="width:400px;"></br>
+    <img src="<?php echo $item[0]; ?>" style="width:40%;"></br>
 <?php else: ?> 
 <?php
     foreach($pictures as $item):
@@ -30,9 +30,9 @@
 ?>
     <div style="float: right; width: 50%">
         <?php echo $item[1] ?>
-        <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><?php include"key.php"?>
+        <!-- <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><?php include"key.php"?> -->
     </div>
-    <img src="<?php echo $item[0]; ?>" style="width:400px;"></br>
+    <img src="<?php echo $item[0]; ?>" style="width:40%;"></br>
 
 <?php endforeach ?>
 <?php endif; ?>
